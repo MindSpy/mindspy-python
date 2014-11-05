df7b7b8ad6cde5993f9917a1f55949216d82f76a
Peter Boraros <pborky@pborky.sk>
2014-11-05 21:48:20 +0100

    plot animation

---
+++ plot.py
@@ -0,0 +1,112 @@
+
+"""
+ldr.py
+
+Display analog data from Arduino using Python (matplotlib)
+
+Author: Mahesh Venkitachalam
+Website: electronut.in
+"""
+from Queue import Empty
+
+from collections import deque
+from numpy import linspace
+from threading import Thread, Event
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from mindspy import Request, Response, CodedSubprocessStream, MatchingStream, MindSpy, State
+from mindspy.matchingstream import synchronized
+
+from time import sleep
+
+class StoppebleLoopDaemonThread(Thread):
+    def __init__(self):
+        super(StoppebleLoopDaemonThread, self).__init__()
+        self.daemon = True
+        self._stop = Event()
+    def stop(self):
+        self._stop.set()
+    def run(self):
+        while not self._stop.is_set():
+            self.loop()
+    def loop(self):
+        raise Exception("Not implemented.")
+
+class MessageConsumer(StoppebleLoopDaemonThread):
+    def __init__(self, gen, buff):
+        super(MessageConsumer, self).__init__()
+        self._buff = buff
+        self._generator = gen
+    def loop(self):
+        try:
+            samples = self._generator.next()
+            with self._buff as buff:
+                for s in samples:
+                    for buf,val in zip(buff,s.payload):
+                        buf.pop()
+                        buf.appendleft(val)
+        except Empty:
+            pass
+
+class Animation(animation.FuncAnimation):
+    def __init__(self, fig, buffer, axes, plotlen, fsamp, **kwargs):
+        self._buffer = buffer
+        self._axes = axes
+        self._plotlen = plotlen
+        self._fsamp = fsamp
+        super(Animation, self).__init__(fig, self.update, **kwargs)
+    def update(self, *a, **kw):
+        with self._buffer as buff:
+            for a,x in zip(self._axes,buff):
+                a.set_data(linspace(0, 1.*self._plotlen/self._fsamp, self._plotlen, endpoint=False), x)
+
+if __name__ == '__main__':
+
+    cs = CodedSubprocessStream('../firmware/test/server', Request, Response)
+    #cs = CodedSerialStream('/dev/ttyACM0', 115200, 1.0, Request, Response)
+
+    dev = MindSpy(MatchingStream(cs))
+    sensors = list(dev.sensors())
+
+    fsamp = 200 # sample rate 200Hz
+    nchan = 4 # 4 channels
+    fsig = 10  # signal frequency 10Hz
+
+    plotlen = 400 # graph width 400px
+    amp = 255 # amplitude 255
+
+    print sensors[0].getModelName().next()
+    states = [
+        State(payload=fsamp, address=0),
+        State(payload=nchan, address=1),
+        State(payload=fsig, address=2),
+        State(payload=amp, address=3)
+    ]
+    sensors[0].setState(states)
+
+    data = sensors[0].getSamples(3, stream=True, timeout=1.0)
+    buffer = synchronized([ deque([0.0]*plotlen) for i in range(nchan) ])
+
+    # set up animation
+    f, axarr = plt.subplots(nchan, sharex=True)
+    for ax in axarr:
+        ax.set_xlim(0, 1.*plotlen/fsamp)
+        ax.set_ylim(-150, 150)
+    axes = [ ax.plot([], [])[0] for ax in axarr ]
+
+    anim = Animation(f, buffer, axes, plotlen, fsamp)
+
+    # start message consumer
+    t = MessageConsumer(data, buffer)
+    t.start()
+
+    # show plot
+    print('plotting data...')
+    try:
+        plt.show()
+    except KeyboardInterrupt:
+        print('exiting')
+
+    t.stop()
+    t.join()
+
